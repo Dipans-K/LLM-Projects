a384f2c0d013c99e99a47421b7b77e85dfc3b77d
Dipans Verma <107676569+Dipans-K@users.noreply.github.com>
2026-09-05 17:56:23 +0530

    Implement EmbeddingModel class using SentenceTransformer

---
+++ embedding.py
@@ -0,0 +1,16 @@
+from sentence_transformers import SentenceTransformer
+
+from src.config import EMBEDDING_MODEL
+
+
+class EmbeddingModel:
+
+    def __init__(self):
+        self.model = SentenceTransformer(EMBEDDING_MODEL)
+
+    def encode(self, texts):
+        return self.model.encode(
+            texts,
+            convert_to_numpy=True,
+            normalize_embeddings=True
+        )
